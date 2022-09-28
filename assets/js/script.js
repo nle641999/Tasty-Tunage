a65f88ba8a437c783f97d863a07c08dad40aadb2
teakoVA <nleb641999@gmail.com>
2022-09-27 19:04:06 -0700

    dont merge

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,54 +18,53 @@ function songSearch(song) {
     "url": searchUrl,
     "method": "GET",
     "headers": {
-    "X-RapidAPI-Key": "6ba9b5ad58mshd483b168ebe03d8p197d0fjsncb9c30ca309d",
-    "X-RapidAPI-Host": "spotify81.p.rapidapi.com"
+      "X-RapidAPI-Key": "6ba9b5ad58mshd483b168ebe03d8p197d0fjsncb9c30ca309d",
+      "X-RapidAPI-Host": "spotify81.p.rapidapi.com"
     }
   };
 
   console.log(searchUrl);
 
   $.ajax(settings).done(function (spotify) {
-    
+
     console.log(spotify);
 
-  }).then(function(spotify) {
-      
-      for (var i = 0; i < spotify.tracks.length; i++) {
-        var songInfo = {
+  }).then(function (spotify) {
+
+    for (var i = 0; i < spotify.tracks.length; i++) {
+      var songInfo = {
         name: spotify.tracks[i].data.name,
         artist: spotify.tracks[i].data.artists.items[0].profile.name,
         album: spotify.tracks[i].data.albumOfTrack.name,
         duration: spotify.tracks[i].data.duration.totalMilliseconds,
-        }
-        //console.log(trackName);
-        var songName = songInfo.name;
-        var songArtist = songInfo.artist;
-        var songAlbum = songInfo.album;
-        var songDuration = songInfo.duration;
-  
-        var tempDuration = moment.duration(songDuration);
-        var inSeconds = tempDuration.seconds();
-        if (inSeconds < 10) {
-          songDuration = tempDuration.minutes() + ':0' + tempDuration.seconds();
-        } else {
-          songDuration = tempDuration.minutes() + ':' + tempDuration.seconds();
-        }
-        
-
-        console.log('Song name: ' + songName + ' , Artist: ' + songArtist 
-                    + ' , Album: ' + songAlbum + ', Duration: ', songDuration);
-        /* console.log(songArtist);
-        console.log(songAlbum);
-        console.log(songDuration); */
       }
-    })
+      //console.log(trackName);
+      var songName = songInfo.name;
+      var songArtist = songInfo.artist;
+      var songAlbum = songInfo.album;
+      var songDuration = songInfo.duration;
+
+      var tempDuration = moment.duration(songDuration);
+      var inSeconds = tempDuration.seconds();
+      if (inSeconds < 10) {
+        songDuration = tempDuration.minutes() + ':0' + tempDuration.seconds();
+      } else {
+        songDuration = tempDuration.minutes() + ':' + tempDuration.seconds();
+      }
+
+
+      console.log('Song name: ' + songName + ' , Artist: ' + songArtist
+        + ' , Album: ' + songAlbum + ', Duration: ', songDuration);
+      /* console.log(songArtist);
+      console.log(songAlbum);
+      console.log(songDuration); */
+    }
+  })
 }
 
-button.on('click', function(event) {
+button.on('click', function (event) {
   event.preventDefault();
 
   var song = $("#search").val();
   songSearch(song);
 })
-
